21f07c981a111900881637b2ffb1aedd6280ce0d
Nana Mohamadi <adilnana78@gmail.com>
2024-11-09 16:18:15 +0100

    Design System, Theming & Tailwind Config

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="flex-center">
+      <h1 className="h1">
+        Hello, I'm only fine you
+      </h1>
+    </div>
   )
 }
 
